d50f0123b7739be1742112541ede836880a426c7
yaoquping <yaoquping@jd..com>
2017-06-01 20:09:17 +0800

    KNN模块实例

---
+++ knn/testKnn.py
@@ -0,0 +1,12 @@
+#-*- coding: UTF-8 -*-
+from knn import *
+import matplotlib
+import matplotlib.pyplot as plt
+# classifyPerson()
+# datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+# fig = plt.figure(1)
+# ax = fig.add_subplot(111)
+# print(type(ax))
+# ax.scatter(datingDataMat[:,0],datingDataMat[:,1],15.0*array(datingLabels),15.0*array(datingLabels))
+# plt.show()
+handwritingClassTest()
